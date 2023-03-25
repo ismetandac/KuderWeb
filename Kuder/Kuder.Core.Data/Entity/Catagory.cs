be1621d98ea0c048bd16eb4c3d4314451e8a469b
ismetandac <ismetandac@gmail.com>
2023-03-25 13:07:09 +0300

    Proje
    Proje

---
+++ Kuder/Kuder.Core.Data/Entity/Catagory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuder.Core.Data.Entity
+{
+    public class Catagory:EntityBase
+    {
+        public int? ParentId { get; set; }
+        public bool IsActive { get; set; }
+        public int OrderNo{ get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Kuder/Kuder.Core.Data/Entity/KuderUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuder.Core.Data.Entity
+{
+    public class KuderUser : EntityBase
+    {
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public string? LastLoginIP { get; set; }
+
+    }
+}
